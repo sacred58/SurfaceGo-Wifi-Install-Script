87d41b104c2c8f2e57592679a7443be1fc8caf44
sacred58 <78624958+sacred58@users.noreply.github.com>
2021-06-13 22:13:07 -0600

    Now using correct comparison operators

--- surface-go-wifi.sh
+++ surface-go-wifi.sh
@@ -3,6 +3,7 @@
 # Because the driver is proprietary, I am not sure I can distribute it on Github. 
 # Source for this information comes from https://www.reddit.com/r/SurfaceLinux/comments/9t53gq/wifi_fixed_on_surface_go_ubuntu_1810/
 # This script expects the board.bin file to be in your user directory
+# Do not run with sudo
 
 # Checks to ensure the binary is in your user directory
 
@@ -10,7 +11,7 @@ driver_present=$(ls ~ | grep 'board.bin')
 
 # If the binary is present, removes the old binaries and puts the new on in it's place
 
-if [ $driver_present == 'board.bin' ]
+if [ $driver_present=='board.bin' ]
 then
     echo "Driver found"
     echo "Continuing"
@@ -28,7 +29,7 @@ fi
 echo "You must restart for changes to take place. Would you like to restart? [Y/N]"
 read restart
 
-if [ $restart == 'Y' ]
+if [ $restart=='Y' ]
 then
     $(shutdown now)
 fi
